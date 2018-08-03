8108e4f5eb79ca05f5e22f56058d1f32d5897fb9
Joel Mikkelsen <jmikkelsen@cbinsights.com>
2018-08-03 11:13:21 -0400

    pre-heroku

--- coffee_air.rb
+++ coffee_air.rb
@@ -5,11 +5,15 @@ require 'json'
 require_relative 'weather'
 require_relative 'coffee'
 
-get '/' do
-  content_type :json
-  @weather = Weather.get_current
-  @score = Weather.outdoor_desireability_score(@weather)
-  @coffee = Coffee.find_coffee(@score['total'])
-  @result = {'coffee'=> @coffee, 'air'=> @weather, 'weather_score'=> @score}
-  @result.to_json
+class CoffeeAir < Sinatra::Base
+
+  get '/' do
+    content_type :json
+    @weather = Weather.get_current
+    @score = Weather.outdoor_desireability_score(@weather)
+    @coffee = Coffee.find_coffee(@score['total'])
+    @result = {'coffee'=> @coffee, 'air'=> @weather, 'weather_score'=> @score}
+    @result.to_json
+  end
+  
 end
---
+++ config.ru
@@ -0,0 +1,2 @@
+require "./coffee_air"
+run CoffeeAir
